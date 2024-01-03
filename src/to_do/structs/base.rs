c3baa2a58baf334360a052666c730b2d9b662bfe
ICavlek <ivan.cavlek1403@gmail.com>
2024-01-03 19:00:54 +0100

    Added unit test for Base - Test SUCCESS

--- src/to_do/structs/base.rs
+++ src/to_do/structs/base.rs
@@ -5,3 +5,20 @@ pub struct Base {
     pub title: String,
     pub status: TaskStatus,
 }
+
+#[test]
+fn test_task_base_values() {
+    let task_pending = Base {
+        title: "Pending".to_string(),
+        status: TaskStatus::Pending,
+    };
+    let task_done = Base {
+        title: "Done".to_string(),
+        status: TaskStatus::Done,
+    };
+    assert_eq!(
+        task_pending.status.stringify(),
+        TaskStatus::Pending.to_string()
+    );
+    assert_eq!(task_done.status.stringify(), TaskStatus::Done.to_string());
+}
