b341eed2efda7caed65f448033e3abc2241a2f59
ICavlek <ivan.cavlek1403@gmail.com>
2024-01-20 16:45:50 +0100

    Restoring main to basic

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,9 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", name)
-}
+use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        println!("Http server factory is firing!");
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-            .route("/say/hello", web::get().to(|| async { "Hello Again!" }))
-    })
-    .bind("127.0.0.1:8080")?
-    .workers(3)
-    .run()
-    .await
+    HttpServer::new(|| App::new())
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
