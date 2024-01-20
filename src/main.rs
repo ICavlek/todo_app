874a2a26a0e0229c117f9385790e8535d8c0422c
ICavlek <ivan.cavlek1403@gmail.com>
2024-01-20 16:51:45 +0100

    Added views into the app

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use actix_web::{App, HttpServer};
+mod views;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new())
+    HttpServer::new(|| App::new().configure(views::views_factory))
         .bind("127.0.0.1:8080")?
         .run()
         .await
