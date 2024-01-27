ec68e425522c989db28e1c29ae661699cd1f03b6
ICavlek <ivan.cavlek1403@gmail.com>
2024-01-27 09:14:17 +0100

    Added apps in views with items function

--- src/views/mod.rs
+++ src/views/mod.rs
@@ -1,3 +1,6 @@
+mod app;
+use app::app_views_factory;
+
 mod auth;
 use auth::auth_views_factory;
 
@@ -7,6 +10,7 @@ use to_do::to_do_views_factory;
 use actix_web::web::ServiceConfig;
 
 pub fn views_factory(app: &mut ServiceConfig) {
+    app_views_factory(app);
     auth_views_factory(app);
     to_do_views_factory(app);
 }
