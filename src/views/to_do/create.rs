16e29594e65d9a0f963cfc6d58e802f35ea88a0b
ICavlek <ivan.cavlek1403@gmail.com>
2024-01-27 14:19:41 +0100

    Refactor create function

--- src/views/to_do/create.rs
+++ src/views/to_do/create.rs
@@ -1,15 +1,16 @@
-use actix_web::HttpRequest;
+use actix_web::{HttpRequest, HttpResponse};
 use serde_json::value::Value;
 use serde_json::Map;
 
+use crate::json_serialization::to_do_items::ToDoItems;
 use crate::processes::process_input;
 use crate::state::read_file;
 use crate::to_do::{enums::TaskStatus, to_do_factory};
 
-pub async fn create(req: HttpRequest) -> String {
+pub async fn create(req: HttpRequest) -> HttpResponse {
     let state: Map<String, Value> = read_file("./state.json");
     let title: String = req.match_info().get("title").unwrap().to_string();
     let item = to_do_factory(title.as_str(), TaskStatus::Pending);
     process_input(item, "create".to_string(), &state);
-    format!("{} created", title)
+    HttpResponse::Ok().json(ToDoItems::get_state())
 }
