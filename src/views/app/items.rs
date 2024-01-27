7e1362383bd6de533b1098bc4ab65ebe3c42ea5e
ICavlek <ivan.cavlek1403@gmail.com>
2024-01-27 16:50:20 +0100

    Migrated javascript code to separate file

--- src/views/app/items.rs
+++ src/views/app/items.rs
@@ -3,7 +3,8 @@ use actix_web::HttpResponse;
 
 pub async fn items() -> HttpResponse {
     let html_data = read_file("./templates/main.html");
+    let javascript_data = read_file("./javascript/main.js");
     HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
-        .body(html_data)
+        .body(html_data.replace("{{JAVASCRIPT}}", &javascript_data))
 }
