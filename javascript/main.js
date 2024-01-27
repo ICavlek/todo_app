b46b3ec19a3197be92e178e6f07e11c951672ae1
ICavlek <ivan.cavlek1403@gmail.com>
2024-01-27 17:32:56 +0100

    Added function for rendering items

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,27 @@
 let createButton = document.getElementById("create-button");
 createButton.addEventListener("click", postAlert);
+
 function postAlert() {
   let titleInput = document.getElementById("name");
   alert(titleInput.value);
   titleInput.value = null;
+}
+
+function renderItems(items, processType, elementId, processFunction) {
+  let itemsMeta = [];
+  let placeholder = "<div>"
+
+  for (let i = 0; i < items.length; i++) {
+    let title = items[i]["title"];
+    let placeholderId = processType + "-" + title.replaceAll(" ", "-");
+    placeholder += "<div>" + title + "<button " + 'id="' + placeholderId + '">' + processType + '</button>' + "</div>";
+    itemsMeta.push({"id": placeholderId, "title": title});
+  }
+
+  placeholder += "</div>"
+  document.getElementById(elementId).innerHTML = placeholder;
+
+  for (let i = 0; i < itemsMeta.length; i++) {
+    document.getElementById(itemsMeta[i]["id"]).addEventListener("click", processFunction);
+  }
 }
