2d121eaf6d10d7e4128f6e590a1f5da6ab61f17f
ICavlek <ivan.cavlek1403@gmail.com>
2024-02-10 20:32:51 +0100

    Added skeleton for CreateToDoItem

---
+++ front_end/src/components/CreateToDoItem.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import axios from "axios";
+
+class CreateToDoItem extends Component {
+  
+  state = {
+    title: ""
+  }
+
+  createItem = () => {
+
+  }
+
+  handleTitleChange = (e) => {
+
+  }
+
+  render() {
+    return (
+
+    )
+  }
+}
+
+export default CreateToDoItem;
