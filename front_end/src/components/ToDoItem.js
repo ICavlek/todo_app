87b9d54f667fa164df31662a0931bc6e8d4152fb
ICavlek <ivan.cavlek1403@gmail.com>
2024-02-10 20:17:05 +0100

    Added skeleton for ToDoItem

---
+++ front_end/src/components/ToDoItem.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import axios from "axios";
+
+class ToDoItem extends Component {
+
+  state = {
+    "title": this.props.title,
+    "status": this.props.status,
+    "button": this.processStatus(this.props.status)
+  }
+
+  processStatus(status) {
+
+  }
+
+  inverseStatus(status) {
+
+  }
+
+  sendRequest = () => {
+
+  }
+
+  render() {
+    return(
+
+    )
+  }
+}
+
+export default ToDoItem;
