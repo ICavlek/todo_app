d7164bbcb2d59db66c50419b1fe1377f0105f21d
ICavlek <ivan.cavlek1403@gmail.com>
2024-02-10 17:41:56 +0100

    Updated get items functionality which calls our backend

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -11,12 +11,26 @@ class App extends Component {
 
   // makes the API call
   getItems() {
-
-  }
+    axios.get(
+      "http://127.0.0.1:8000/v1/item/get",
+      {headers: {"token": "some_token"}}
+    ).then(
+      response => {
+        let pending_items = response.data["pending_items"];
+        let done_items = response.data["done_items"];
+        this.setState({
+          "pending_items": this.processItemValues(pending_items),
+          "done_items": this.processItemValues(done_items),
+          "pending_items_count": response.data["pending_item_count"],
+          "done_items_count": response.data["done_item_count"]
+        });
+      }
+    )
+  };
 
   // ensures the API call is updated when mounted
   componentDidMount() {
-
+    this.getItems();
   }
 
   // convert items from API to HTML
