503b1b2ac0b8a6b8d55ba3c52a2b47bf6b3fb494
ICavlek <ivan.cavlek1403@gmail.com>
2024-02-10 20:37:19 +0100

    Implemented render part

--- front_end/src/components/CreateToDoItem.js
+++ front_end/src/components/CreateToDoItem.js
@@ -17,7 +17,12 @@ class CreateToDoItem extends Component {
 
   render() {
     return (
-
+      <div className="inputContainer">
+        <input type="text" id="name" placeholder="create to do item" value={this.state.title} onChange={this.handleTitleChange}/>
+        <div className="actionButton" id="create-button" onClick={this.createItem}>
+          Create
+        </div>
+      </div>
     )
   }
 }
