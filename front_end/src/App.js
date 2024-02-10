3230a7a7452c260f4924771e21d903c7de825b93
ICavlek <ivan.cavlek1403@gmail.com>
2024-02-10 17:07:36 +0100

    Empty skeleton of the application

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
   state = {
-    "message": "To Do"
+    "pending_items": [],
+    "done_items": [],
+    "pending_items_count": 0,
+    "done_items_count": 0
   }
+
+  // makes the API call
+  getItems() {
+
+  }
+
+  // ensures the API call is updated when mounted
+  componentDidMount() {
+
+  }
+
+  // convert items from API to HTML
+  processItemValues(items) {
+
+  }
+
+  // returns the HTML to be rendered
   render() {
     return (
       <div className="App">
-      <p>{this.state.message} application</p>
+      <p>To do application</p>
       </div>
     )
   }
