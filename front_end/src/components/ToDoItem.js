2b466023e974529b117a1e0e88dfe1d0c9e337a0
ICavlek <ivan.cavlek1403@gmail.com>
2024-02-10 20:28:20 +0100

    Implemented ToDoItem component

--- front_end/src/components/ToDoItem.js
+++ front_end/src/components/ToDoItem.js
@@ -10,20 +10,40 @@ class ToDoItem extends Component {
   }
 
   processStatus(status) {
-
+    if (status === "PENDING") {
+      return "edit"
+    } else {
+      return "delete"
+    }
   }
 
   inverseStatus(status) {
-
+    if (status === "PENDING") {
+      return "DONE"
+    } else {
+      return "PENDING"
+    }
   }
 
   sendRequest = () => {
-
+    axios.post(
+      "http://127.0.0.1:8000/v1/item/" + this.state.button,
+      {
+        "title": this.state.title,
+        "status": this.inverseStatus(this.state.status)
+      },
+      {headers: {"token": "some_token"}}
+    ).then(response => {
+      this.props.passBackResponse(response);
+    });
   }
 
   render() {
     return(
-
+      <div>
+        <p>{this.state.title}</p>
+        <button onClick={this.sendRequest}>{this.state.button}</button>
+      </div>
     )
   }
 }
