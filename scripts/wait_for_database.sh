0173cd78493cbb930334d3440b23ea05c4d1c483
ICavlek <ivan.cavlek1403@gmail.com>
2024-02-28 19:01:01 +0100

    Script for foundations to build pipelines, tests and onboarding packages to start development

---
+++ scripts/wait_for_database.sh
@@ -0,0 +1,11 @@
+cd ..
+docker-compose up -d
+
+until pg_isready -h localhost -p 5432 -U username
+do
+  echo "Waiting for postgres"
+  sleep 2;
+done
+
+echo "docker is now running"
+docker-compose down
